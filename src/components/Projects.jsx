118c6b6db423b80e7301bafa893a1e22647ce551
frealis <frealis@asdf.com>
2019-06-19 13:52:53 -0400

    Change 'projects-project-...' class syntax to 'projects-thumbnail-...' in _projects.scss and Projects.jsx

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -20,16 +20,16 @@ class Projects extends Component {
               <figure>
                 <svg className="bd-placeholder-img" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Thumbnail"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/></svg>
                 <figcaption className="d-flex flex-column justify-content-between">
-                  <p className="projects-project-description">Residential and commercial pressure washing services</p>
+                  <p className="projects-thumbnail-description">Residential and commercial pressure washing services</p>
                   <div className="d-flex btn-group justify-content-center">
                     <button className="btn btn-sm btn-view">View</button>
                     <button className="btn btn-sm btn-source">Source</button>
                   </div>
-                  <div className="d-flex projects-project-icons justify-content-around">
-                    <img className="projects-project-icon" alt="Bootstrap" src={ IconBootstrap } />
-                    <img className="projects-project-icon" alt="Django" src={ IconDjango } />
-                    <img className="projects-project-icon" alt="Postgres" src={ IconPostgres } />
-                    <img className="projects-project-icon" alt="AWS" src={ IconAWS } />
+                  <div className="d-flex projects-thumbnail-icons justify-content-around">
+                    <img className="projects-thumbnail-icon" alt="Bootstrap" src={ IconBootstrap } />
+                    <img className="projects-thumbnail-icon" alt="Django" src={ IconDjango } />
+                    <img className="projects-thumbnail-icon" alt="Postgres" src={ IconPostgres } />
+                    <img className="projects-thumbnail-icon" alt="AWS" src={ IconAWS } />
                   </div>
                 </figcaption>
               </figure>
