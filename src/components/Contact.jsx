b448fe0c9546314ee13042406689763d25e2344b
frealis <mhh129@gmail.com>
2019-06-21 20:25:48 -0400

    Move around elements in Contact.jsx -- email link is now next to the 'Send Message' for the contact form

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -6,38 +6,57 @@ import { ReactComponent as IconLinkedIn } from '../img/LinkedIn-043546.svg';
 
 class Contact extends Component {
   render() {
-
-    // Destructure this.props
-    // const { IconEmail, IconGitHub, IconLinkedIn } = this.props;
-
     return (
       <div className="contact">
         <div id="anchor-contact" className="anchor"></div>
         <h1>Contact</h1>
         <div className="container">
+
+          {/* Contact Form */}
           <div className="row">
-            <div className="col-md-6">
+            <div className="col-md-1"></div>
+            <div className="col-md-10">
               <form action="https://formspree.io/email@domain.tld" method="POST">
                 <div className="contact-input">
-                  <input name="contact_name" id="contact-name" className="contact-input-field" type="text" required />
+                  <input name="contact_name" id="contact-name-input" className="contact-input-field" type="text" required />
                   <label htmlFor="contact-name" className="floating-placeholder">Name</label>
                 </div>
                 <div className="contact-input">
-                  <input name="contact_email" id="contact-email" className="contact-input-field" type="text" required />
+                  <input name="contact_email" id="contact-email-input" className="contact-input-field" type="text" required />
                   <label htmlFor="contact-email" className="floating-placeholder">Email</label>
                 </div>
                 <div className="contact-input">
-                  <textarea name="contact_message" id="contact-message" className="contact-input-field" cols="30" rows="5" required></textarea>
+                  <textarea name="contact_message" id="contact-message-input" className="contact-input-field" cols="30" rows="5" required></textarea>
                   <label htmlFor="contact-message" className="floating-placeholder">Message</label>
                 </div>
               </form>
-              <button id="contact-button-send" className="btn btn-sm">Send</button>
+
+              <div className="d-flex justify-content-around">
+                <button id="contact-button-send" className="btn btn-sm">Send Message</button>
+                <div className="contact-button-and-email-links">
+                  <div className="d-flex align-items-center contact-link contact-link-email">
+                    <IconEmail className="contact-icon" /> <span className="contact-email-address">mhh129@gmail.com</span>
+                  </div>
+                  <div className="clecm-reference-point">
+                    <button className="clecm-button">
+                      <IconCopy className="contact-link-email-copy" />
+                    </button>
+                    <span className="clecm-message clecm-info">Click to copy</span>
+                    <span className="clecm-message clecm-success">Copied!</span>
+                  </div>
+                </div>
+              </div>
+
+            
+            
             </div>
-
             <div className="col-md-1"></div>
+          </div>
 
+          {/* Contact Links */}
+          <div className="row">
             <div className="col-md-5">
-              <div className="d-flex align-items-center contact-link contact-link-email">
+              {/* <div className="d-flex align-items-center contact-link contact-link-email">
                 <IconEmail className="contact-icon" /> <span className="contact-email-address">mhh129@gmail.com</span>
               </div>
               <div className="clecm-reference-point">
@@ -46,7 +65,7 @@ class Contact extends Component {
                 </button>
                 <span className="clecm-message clecm-info">Click to copy</span>
                 <span className="clecm-message clecm-success">Copied!</span>
-              </div>
+              </div> */}
               <div className="d-flex align-items-center contact-link contact-link-github">
                 <IconGitHub className="contact-icon" /> GitHub
               </div>
