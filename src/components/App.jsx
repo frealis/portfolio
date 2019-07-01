dadbeeba6c66abb8da0fc523dea1900c17c52f7f
frealis <mhh129@gmail.com>
2019-07-01 16:09:00 -0400

    Change external_links.js into a React component in App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -22,7 +22,7 @@ import IconReact from '../img/React-icon2.svg';
 import IconResume from '../img/custom-resume-043546.svg';
 import IconSass from '../img/Sass_Logo_Color.svg';
 
-import '../js/external_links';
+import ExternalJS from '../js/external_links';
 
 class App extends Component {
   state = {
@@ -41,6 +41,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <ExternalJS />
         <Navbar toggleHamburger={ this.toggleHamburger } IconLinkedIn={ IconLinkedIn } IconResume={ IconResume } />
         <Main IconAWS={ IconAWS } IconBootstrap={ IconBootstrap } IconCopy={ IconCopy } IconCSS={ IconCSS } IconDjango={ IconDjango } IconEmail={ IconEmail } IconFlask={ IconFlask } IconGitHub={ IconGitHub } IconHackerRank={ IconHackerRank } IconHeroku={ IconHeroku } IconHTML={ IconHTML } IconJavaScript={ IconJavaScript} IconLinkedIn={ IconLinkedIn } IconPostgres={ IconPostgres } IconPython={ IconPython } IconReact={ IconReact } IconResume={ IconResume } IconSass={ IconSass } />
         <Footer IconEmail={ IconEmail } IconGitHub={ IconGitHub } IconHackerRank={ IconHackerRank } IconLinkedIn={ IconLinkedIn } IconResume={ IconResume } />
