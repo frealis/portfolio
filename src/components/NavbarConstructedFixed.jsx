fa66357a0fa078ecee13a814c8c1254f29f89cd9
matthigg <mhh129@gmail.com>
2019-08-04 13:11:52 -0400

    Add <NavbarConstructedFixed />

---
+++ src/components/NavbarConstructedFixed.jsx
@@ -0,0 +1,11 @@
+// Navbar Constructed Fixed
+<nav class="nav-fixed scraps-collected nav-fixed-vanish">
+  <div class="scrap-collected fixed brand">      
+    Mustachio the Pistachio
+    <span class="brand-sub">Web Developer</span>
+  </div>
+  <div class="scrap-collected fixed links">
+    <a class="scrap-collected fixed link-projects">Projects</a>
+    <a class="scrap-collected fixed link-contact">Contact</a>
+  </div>
+</nav>
