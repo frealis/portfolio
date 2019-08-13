493559243880d39cf7ebd86a1be5e2116fcd8d85
matthigg <mhh129@gmail.com>
2019-08-13 11:14:19 -0400

    Fix navbar.js (supposed to hide bootstrap when window.innderWidth > 768, not < 768)

--- src/js/navbar.js
+++ src/js/navbar.js
@@ -2,12 +2,12 @@ import React from 'react'
 
 export function navbar() {
   
-  window.innerWidth < 768
+  window.innerWidth > 768
     ? document.querySelector('.navbar').classList.add('display-none')
     : document.querySelector('.navbar').classList.remove('display-none')
 
   window.onresize = () => {
-    window.innerWidth < 768
+    window.innerWidth > 768
       ? document.querySelector('.navbar').classList.add('display-none')
       : document.querySelector('.navbar').classList.remove('display-none')
   }
