91371b97f166a0c403c642e3a2af3eca2aa4aad7
matthigg <mhh129@gmail.com>
2019-08-14 18:05:51 -0400

    Adjust quinary color, change background color to primary color

--- src/js/colors-shuffled.js
+++ src/js/colors-shuffled.js
@@ -5,7 +5,9 @@ export function colorsShuffled() {
     '#FCFAED',
     '#043546',
     '#ECD4B0',
-    '#1B969E',
+    // '#1B969E',
+    // '#0A8582',
+    '#097471',
   ]
   colors.forEach(() => {
     let random_1 = Math.floor(Math.random() * colors.length);
