6f7dda7a1610d2c25d5691baae0f07580c96d904
matthigg <mhh129@gmail.com>
2019-08-14 12:17:10 -0400

    Remove the DOM-tree search from navbar.js for the hamburger icon, add function to navbar links that collapse the navbar onclick

--- src/js/navbar.js
+++ src/js/navbar.js
@@ -1,6 +1,6 @@
 export function navbar() {
   
-  // Hide NavbarSidePanel.jsx if window width is > 768px, otherwise show it
+  // Collapse NavbarSidePanel.jsx if window width is > 768px, otherwise expand it
   window.innerWidth > 768
     ? document.querySelector('.navbar').classList.add('display-none')
     : document.querySelector('.navbar').classList.remove('display-none')
@@ -11,32 +11,28 @@ export function navbar() {
       : document.querySelector('.navbar').classList.remove('display-none')
   }
 
+  const toggle = document.querySelector('.nav-btn-toggle-sidepanel').dataset.toggle
+  const target = document.querySelector('.nav-btn-toggle-sidepanel').dataset.target
+
   // Add hamburger icon toggle functionality
-  document.querySelector('.nav-btn-toggle-sidepanel').addEventListener('click', (e) => {
-    let toggle = ''
-    let target = ''
-    if (e.target.dataset.toggle) {
-      toggle = e.target.dataset.toggle
-      target = e.target.dataset.target
-    } else {
-      let currentNode = e.target    
-      while (currentNode.parentNode) {
-        if (currentNode.dataset.toggle) {
-          toggle = currentNode.dataset.toggle
-          target = currentNode.dataset.target
-        }
-        currentNode = currentNode.parentNode
-      }
-    }
+  document.querySelector('.nav-btn-toggle-sidepanel').addEventListener('click', () => {
     let token_exists = false
-    document.querySelector(`${target}`).classList.forEach(token => {
+    document.querySelector(target).classList.forEach(token => {
       if (token === toggle) {
         token_exists = true
       }
     })
     token_exists
-      ? document.querySelector(`${target}`).classList.remove(toggle)
-      : document.querySelector(`${target}`).classList.add(toggle)
+      ? document.querySelector(target).classList.remove(toggle)
+      : document.querySelector(target).classList.add(toggle)
   })
+
+  // Collapse NavbarSidePanel.jsx if a navbar link is clicked
+  const nav_links = document.querySelectorAll('.nav-link')
+  nav_links.forEach(link => {
+    link.addEventListener('click', () => {
+      document.querySelector(target).classList.add(toggle)
+    })
+  }) 
 }  
 
