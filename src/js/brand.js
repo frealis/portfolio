3d1ed58c75a57a288411fd95167d4c01c05b991f
matthigg <mhh129@gmail.com>
2019-08-14 22:14:56 -0400

    Make brand always collapse on screen width < 768px

--- src/js/brand.js
+++ src/js/brand.js
@@ -1,8 +1,24 @@
 export function brand() {
+
+  // Minimize brand when user scrolls down
   const main = document.querySelector('.main')
   main.onscroll = () => {
-    main.scrollTop > 0
-      ? document.querySelector('.brand').classList.add('miniaturized')
-      : document.querySelector('.brand').classList.remove('miniaturized')
+    if (main.scrollTop === 0 && window.innerWidth >= 768) {
+      document.querySelector('.brand').classList.remove('miniaturized')
+    } else {
+      document.querySelector('.brand').classList.add('miniaturized')
+    }
+  }
+
+  // Minimize brand when screen size is < 768px wide
+  console.log(window.innerWidth)
+  window.innerWidth >= 768
+    ? document.querySelector('.brand').classList.remove('miniaturized')
+    : document.querySelector('.brand').classList.add('miniaturized')
+
+  window.onresize = () => {
+    window.innerWidth >= 768
+      ? document.querySelector('.brand').classList.remove('miniaturized')
+      : document.querySelector('.brand').classList.add('miniaturized')
   }
 }
