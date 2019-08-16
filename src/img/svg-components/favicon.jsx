fe6ca9776f81d23fd246557c2513b2cfc5ddf060
matthigg <mhh129@gmail.com>
2019-08-15 20:46:58 -0400

    Update favicon.ico

---
+++ src/img/svg-components/favicon.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function favicon() {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 110 110">
+      <g transform="translate(-1397 -2695)">
+        <circle cx="50" cy="50" r="50" transform="translate(1402 2700)" strokeWidth="6"/>
+        <path d="M39.507,0H32.5V-18.853q0-3.054.264-6.746h-.176a33.194,33.194,0,0,1-.989,4.175L24.214,0h-5.8L10.9-21.2A41.26,41.26,0,0,1,9.91-25.6h-.2Q10-20.94,10-17.424V0H3.6V-31.509H14l6.438,18.677a26.3,26.3,0,0,1,1.121,4.5h.132a45.356,45.356,0,0,1,1.252-4.548l6.438-18.633H39.507ZM70.564,0H63.445V-12.832H50.393V0H43.3V-31.509h7.1V-18.94H63.445V-31.509h7.119Z" transform="translate(1413.784 2766.856)"/>
+      </g>
+    </svg>
+  )
+}
